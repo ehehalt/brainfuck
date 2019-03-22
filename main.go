6c2a6d75e629f4ceea5cf9663595f6e1e47c3a29
Mike <mike42@hotmail.de>
2019-03-22 20:00:48 +0100

    First running version

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "io/ioutil"
+  "os"
+
+  "github.com/ehehalt/brainfuck.go/machine"
+)
 
 func main() {
-	fmt.Println("Brainf*ck")
+  fmt.Println("Brainf*ck")
+  fileName := os.Args[1]
+  code, err := ioutil.ReadFile(fileName)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "error: %s\n", err)
+    os.Exit(-1)
+  }
+
+  m := machine.NewMachine(string(code), os.Stdin, os.Stdout)
+  m.Execute()
 }
